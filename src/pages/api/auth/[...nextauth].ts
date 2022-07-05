075f4cad6f4e7afb7399023a427c2ffb4493bc9f
Thomas <tgusewelle@gkwmedia.com>
2022-07-05 14:47:45 -0500

    initial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId:
+        "167628653049-r3phkr8ao9fmvn5kjquejtcb1ub1eb4o.apps.googleusercontent.com",
+      clientSecret: "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",
+      // clientId: process.env.GOOGLE_ID || "",
+      // clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+    // ...add more providers here
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+      },
+      async authorize(credentials, _req) {
+        const user = { id: 1, name: credentials?.name ?? "J Smith" };
+        return user;
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);

2b20435635b1663e6a02661ee19bd166b1e63023
Thomas <tgusewelle@gkwmedia.com>
2022-07-18 13:57:46 -0500

    change roles to include the organization they from

--- src/server/router/role.ts
+++ src/server/router/role.ts
@@ -1,7 +1,18 @@
+import { truncate } from "fs";
 import { createRouter } from "./context";
 
-export const roleRouter = createRouter().query("getRoles", {
+export const roleRouter = createRouter().query("getRolesByOrganization", {
   async resolve({ ctx }) {
-    return await prisma?.role.findMany();
+    const org = await prisma?.user.findFirst({
+      select: {
+        organizationId: true,
+      },
+      where: {
+        id: ctx.session?.user.id,
+      },
+    });
+    return await prisma?.role.findMany({
+      where: { organizationId: org?.organizationId },
+    });
   },
 });

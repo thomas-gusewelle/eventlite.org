ed5a852f21d23875e7142178f71fdc3fdfa25535
Thomas <tgusewelle@gkwmedia.com>
2022-07-18 11:32:01 -0500

    locations page with filtering

--- src/pages/locations.tsx
+++ src/pages/locations.tsx
@@ -100,7 +100,17 @@ const LocationsPage = () => {
     }
   });
 
-  const onDelete = (location: Locations) => {};
+  const filter = (e: string) => {
+    if (e.length > 0) {
+      let key = e.toLowerCase();
+      const filter = locations.data?.filter((loc) => {
+        return loc.name?.toLowerCase().includes(key);
+      });
+      setLocationList(filter);
+    } else {
+      setLocationList(locations.data);
+    }
+  };
 
   return (
     <>
@@ -165,7 +175,7 @@ const LocationsPage = () => {
           <SectionHeading>Locations</SectionHeading>
           <div className='flex gap-4'>
             <input
-              // onChange={(e) => filter(e.target.value)}
+              onChange={(e) => filter(e.target.value)}
               className='border border-gray-100 focus:outline-none focus:border-indigo-700 rounded-xl w-full text-sm text-gray-500 bg-gray-100 pl-4 py-2'
               type='text'
               placeholder='Search'

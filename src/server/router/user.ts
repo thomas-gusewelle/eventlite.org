b373c5f6b4e5690ba0a91c725163f98292ffb6c4
tgusewelle <tgusewelle@gkwmedia.com>
2022-07-21 18:22:25 -0500

    sorting by user name

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -4,126 +4,127 @@ import { z } from "zod";
 import { UserStatus } from "@prisma/client";
 
 export const userRouter = createRouter()
-  .query("getUser", {
-    async resolve({ ctx }) {
-      const userInfo = await prisma?.user.findFirst({
-        where: {
-          id: ctx.session?.user.id,
-        },
-      });
+	.query("getUser", {
+		async resolve({ ctx }) {
+			const userInfo = await prisma?.user.findFirst({
+				where: {
+					id: ctx.session?.user.id,
+				},
+			});
 
-      return userInfo;
-    },
-  })
-  .query("getUserByID", {
-    input: z.string(),
-    async resolve({ input }) {
-      return await prisma?.user.findFirst({
-        where: {
-          id: input,
-        },
-        include: { roles: true },
-      });
-    },
-  })
-  .query("getUsersByOrganization", {
-    async resolve({ ctx }) {
-      const orgID = await prisma?.user.findFirst({
-        where: { id: ctx.session?.user.id },
-        select: { organizationId: true },
-      });
+			return userInfo;
+		},
+	})
+	.query("getUserByID", {
+		input: z.string(),
+		async resolve({ input }) {
+			return await prisma?.user.findFirst({
+				where: {
+					id: input,
+				},
+				include: { roles: true },
+			});
+		},
+	})
+	.query("getUsersByOrganization", {
+		async resolve({ ctx }) {
+			const orgID = await prisma?.user.findFirst({
+				where: { id: ctx.session?.user.id },
+				select: { organizationId: true },
+			});
 
-      return await prisma?.user.findMany({
-        where: {
-          organizationId: orgID?.organizationId,
-        },
-        include: {
-          roles: true,
-        },
-      });
-    },
-  })
-  .query("getAmdminCount", {
-    async resolve({ ctx }) {
-      const orgID = await prisma?.user.findFirst({
-        where: { id: ctx.session?.user.id },
-        select: { organizationId: true },
-      });
-      return await prisma?.user.count({
-        where: { status: "ADMIN" },
-      });
-    },
-  })
+			return await prisma?.user.findMany({
+				orderBy: [{ lastName: "asc" }, { firstName: "asc" }],
+				where: {
+					organizationId: orgID?.organizationId,
+				},
+				include: {
+					roles: true,
+				},
+			});
+		},
+	})
+	.query("getAmdminCount", {
+		async resolve({ ctx }) {
+			const orgID = await prisma?.user.findFirst({
+				where: { id: ctx.session?.user.id },
+				select: { organizationId: true },
+			});
+			return await prisma?.user.count({
+				where: { status: "ADMIN" },
+			});
+		},
+	})
 
-  .mutation("addUser", {
-    input: z.object({
-      firstName: z.string(),
-      lastName: z.string(),
-      email: z.string(),
-      role: z.object({ id: z.string(), name: z.string() }).array(),
-      status: z.string(),
-    }),
+	.mutation("addUser", {
+		input: z.object({
+			firstName: z.string(),
+			lastName: z.string(),
+			email: z.string(),
+			role: z.object({ id: z.string(), name: z.string() }).array(),
+			status: z.string(),
+		}),
 
-    async resolve({ ctx, input }) {
-      const orgID = await prisma?.user.findFirst({
-        where: { id: ctx.session?.user.id },
-        select: { organizationId: true },
-      });
-      console.log(input.status);
-      return await prisma?.user.create({
-        data: {
-          firstName: input.firstName,
-          lastName: input.lastName,
-          email: input.email,
-          organizationId: orgID?.organizationId,
-          roles: {
-            connect: input.role.map((role) => ({
-              id: role.id,
-            })),
-          },
-          status: input.status as UserStatus,
-        },
-      });
-    },
-  })
-  .mutation("updateUserByID", {
-    input: z.object({
-      id: z.string(),
-      firstName: z.string(),
-      lastName: z.string(),
-      email: z.string(),
-      role: z.object({ id: z.string(), name: z.string() }).array(),
-      status: z.string(),
-    }),
+		async resolve({ ctx, input }) {
+			const orgID = await prisma?.user.findFirst({
+				where: { id: ctx.session?.user.id },
+				select: { organizationId: true },
+			});
+			console.log(input.status);
+			return await prisma?.user.create({
+				data: {
+					firstName: input.firstName,
+					lastName: input.lastName,
+					email: input.email,
+					organizationId: orgID?.organizationId,
+					roles: {
+						connect: input.role.map((role) => ({
+							id: role.id,
+						})),
+					},
+					status: input.status as UserStatus,
+				},
+			});
+		},
+	})
+	.mutation("updateUserByID", {
+		input: z.object({
+			id: z.string(),
+			firstName: z.string(),
+			lastName: z.string(),
+			email: z.string(),
+			role: z.object({ id: z.string(), name: z.string() }).array(),
+			status: z.string(),
+		}),
 
-    async resolve({ input }) {
-      const allRoles = await prisma?.role.findMany({});
-      const disconnectRoles = allRoles?.filter((role) =>
-        input.role.every((i) => i.id !== role.id)
-      );
+		async resolve({ input }) {
+			const allRoles = await prisma?.role.findMany({});
+			const disconnectRoles = allRoles?.filter((role) =>
+				input.role.every((i) => i.id !== role.id)
+			);
 
-      return await prisma?.user.update({
-        data: {
-          firstName: input.firstName,
-          lastName: input.lastName,
-          email: input.email,
-          roles: {
-            connect: input.role.map((role) => ({
-              id: role.id,
-            })),
-            disconnect: disconnectRoles?.map((role) => ({ id: role.id })),
-          },
-          status: input.status as UserStatus,
-        },
-        where: { id: input.id },
-      });
-    },
-  })
-  .mutation("deleteUserByID", {
-    input: z.string(),
-    async resolve({ input }) {
-      return await prisma?.user.delete({
-        where: { id: input },
-      });
-    },
-  });
+			return await prisma?.user.update({
+				data: {
+					firstName: input.firstName,
+					lastName: input.lastName,
+					email: input.email,
+					roles: {
+						connect: input.role.map((role) => ({
+							id: role.id,
+						})),
+						disconnect: disconnectRoles?.map((role) => ({ id: role.id })),
+					},
+					status: input.status as UserStatus,
+				},
+				where: { id: input.id },
+			});
+		},
+	})
+	.mutation("deleteUserByID", {
+		input: z.string(),
+		async resolve({ input }) {
+			return await prisma?.user.delete({
+				where: { id: input },
+			});
+		},
+	});

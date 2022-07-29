2cff271ea62d8b2d9120afc436e858a376c164bc
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2022-07-29 08:01:14 -0500

    fixed issue with loading on submit and  schema

--- src/server/router/events.ts
+++ src/server/router/events.ts
@@ -103,6 +103,7 @@ export const eventsRouter = createRouter()
       return await prisma?.event.create({
         data: {
           name: input.name,
+          recurringId: input?.recurringId,
           datetime: replaceTime(input.eventDate, input.eventTime),
           organizationId: org?.organizationId,
           locationsId: input.eventLocation.id,
@@ -115,113 +116,6 @@ export const eventsRouter = createRouter()
       });
     },
   })
-
-  // .mutation("createRecurringEvents", {
-  //   input: z.object({
-  //     name: z.string(),
-  //     eventDate: z.date(),
-  //     eventTime: z.date(),
-  //     eventLocation: z.object({
-  //       id: z.string(),
-  //       name: z.string(),
-  //       organizationId: z.string(),
-  //     }),
-  //     isRepeating: z.boolean(),
-  //     repeatFrequency: z
-  //       .object({
-  //         id: z.union([
-  //           z.literal("D"),
-  //           z.literal("W"),
-  //           z.literal("WC"),
-  //           z.literal("M"),
-  //         ]),
-  //         name: z.string(),
-  //       })
-  //       .optional(),
-  //     DEndSelect: z
-  //       .object({
-  //         id: z.union([z.literal("Num"), z.literal("Date")]),
-  //         name: z.string(),
-  //       })
-  //       .optional(),
-  //     DNum: z.number().optional(),
-  //     DDate: z.date().optional(),
-  //     WEndSelect: z
-  //       .object({
-  //         id: z.union([z.literal("Num"), z.literal("Date")]),
-  //         name: z.string(),
-  //       })
-  //       .optional(),
-  //     WNum: z.number().optional(),
-  //     WDate: z.date().optional(),
-  //     WCEndSelect: z
-  //       .object({
-  //         id: z.union([z.literal("Num"), z.literal("Date")]),
-  //         name: z.string(),
-  //       })
-  //       .optional(),
-  //     WCNum: z.number().optional(),
-  //     WCDate: z.date().optional(),
-  //     WCSun: z.boolean().optional(),
-  //     WCMon: z.boolean().optional(),
-  //     WCTues: z.boolean().optional(),
-  //     WCWed: z.boolean().optional(),
-  //     WCThurs: z.boolean().optional(),
-  //     WCFri: z.boolean().optional(),
-  //     WCSat: z.boolean().optional(),
-  //     MEndSelect: z
-  //       .object({
-  //         id: z.union([z.literal("Num"), z.literal("Date")]),
-  //         name: z.string(),
-  //       })
-  //       .optional(),
-  //     MNum: z.number().optional(),
-  //     MDate: z.date().optional(),
-  //     positions: z
-  //       .object({
-  //         position: z.object({
-  //           id: z.string(),
-  //           name: z.string(),
-  //           organizationId: z.string().optional(),
-  //         }),
-  //         quantity: z.number(),
-  //       })
-  //       .array(),
-  //   }),
-
-  //   async resolve({ ctx, input }) {
-  //     if (input.isRepeating == false) {
-  //       throw new TRPCError({ code: "BAD_REQUEST" });
-  //       return;
-  //     }
-  //     const org = await prisma?.user.findFirst({
-  //       where: { id: ctx.session?.user.id },
-  //       select: { organizationId: true },
-  //     });
-
-  //     const formatedDate = replaceTime(input.eventDate, input.eventTime);
-  //     const newDates = findFutureDates(input);
-
-  //     const events = newDates?.map(async (date) => {
-  //       await prisma?.event.create({
-  //         data: {
-  //           name: input.name,
-  //           datetime: date,
-  //           organizationId: org?.organizationId,
-  //           locationsId: input.eventLocation.id,
-  //           positions: {
-  //             create: input.positions.map((item) => ({
-  //               roleId: item.position.id,
-  //             })),
-  //           },
-  //         },
-  //       });
-  //     });
-
-  //     return events;
-  //   },
-  // })
-
   .mutation("deleteEventById", {
     input: z.string(),
     async resolve({ input }) {

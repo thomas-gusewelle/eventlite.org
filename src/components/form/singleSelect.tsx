83db7e8340a6f592a27a8fe5cbbeaf65b7608864
Thomas <tgusewelle@gkwmedia.com>
2022-08-03 08:54:49 -0500

    exposed value of single select setState

--- src/components/form/singleSelect.tsx
+++ src/components/form/singleSelect.tsx
@@ -12,7 +12,7 @@ export const SingleSelect: React.FC<{
 
   return (
     <div className=' focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 border rounded-md'>
-      <Listbox value={selected} onChange={setSelected}>
+      <Listbox value={selected} onChange={(value) => setSelected(value)}>
         <div className='relative mt-1 '>
           <Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
             <div className='flex flex-wrap gap-2 min-h-[1rem]'>

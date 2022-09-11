54acaa9b011614f1e3294721a3148e47b761e08f
Thomas <tgusewelle@gkwmedia.com>
2022-09-11 10:38:20 -0500

    created query and create many mutation

--- src/server/router/availability.ts
+++ src/server/router/availability.ts
@@ -4,4 +4,27 @@ import { z } from "zod";
 import { UserStatus } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 
-export const avalibiltyRouter = createRouter();
+export const avalibiltyRouter = createRouter()
+  .query("getUserAvalibility", {
+    async resolve({ ctx }) {
+      return await prisma?.availability.findMany({
+        where: {
+          id: ctx.session?.user.id,
+          date: {
+            gte: new Date(),
+          },
+        },
+      });
+    },
+  })
+  .mutation("createUserAvalibility", {
+    input: z.date().array(),
+    async resolve({ ctx, input }) {
+      return await prisma?.availability.createMany({
+        data: input.map((date) => ({
+          userId: ctx.session?.user.id,
+          date: date,
+        })),
+      });
+    },
+  });

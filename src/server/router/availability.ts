48bb44b406b95a6fba2461faa329e07cefc7b8f3
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2022-09-11 15:34:10 -0500

    updated mutations

--- src/server/router/availability.ts
+++ src/server/router/availability.ts
@@ -1,8 +1,9 @@
 import { createRouter } from "./context";
-import { z } from "zod";
+import { date, z } from "zod";
 
 import { UserStatus } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
+import { resolve } from "path";
 
 export const avalibiltyRouter = createRouter()
   .query("getUserAvalibility", {
@@ -17,14 +18,36 @@ export const avalibiltyRouter = createRouter()
       });
     },
   })
-  .mutation("createUserAvalibility", {
-    input: z.date().array(),
+  .mutation("updateUserAvalibility", {
+    input: z.object({
+      newDates: z.date().array(),
+      deleteDates: z.date().array(),
+    }),
     async resolve({ ctx, input }) {
-      return await prisma?.availability.createMany({
-        data: input.map((date) => ({
+      const newDates = await prisma?.availability.createMany({
+        data: input.newDates.map((date) => ({
           userId: ctx.session?.user.id,
           date: date,
         })),
+      });
+      const deleteDates = await prisma?.availability.deleteMany({
+        where: {
+          userId: ctx.session?.user.id,
+          date: {
+            in: input.deleteDates,
+          },
+        },
+      });
+      return { newDates: newDates, deletedDates: deleteDates };
+    },
+  })
+  .mutation("deleteDate", {
+    input: z.string(),
+    async resolve({ input }) {
+      return await prisma?.availability.delete({
+        where: {
+          id: input,
+        },
       });
     },
   });

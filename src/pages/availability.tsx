c416e97820073a60fcf028a7b0c6b5d05355a989
tgusewelle <tgusewelle@gkwmedia.com>
2022-09-24 13:45:51 -0500

    updates list when deleting from dropdown

--- src/pages/availability.tsx
+++ src/pages/availability.tsx
@@ -16,105 +16,111 @@ import { paginate } from "../utils/paginate";
 import { trpc } from "../utils/trpc";
 
 const AvailabilityPage = () => {
-  const [limit, setLimit] = useState(4);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [dates, setDates] = useState<Availability[]>([]);
-  const [pagiantedData, setpagiantedData] =
-    useState<PaginateData<Availability[]>>();
-  const [pageNum, setPageNum] = useState(1);
+	const [limit, setLimit] = useState(4);
+	const [modalOpen, setModalOpen] = useState(false);
+	const [dates, setDates] = useState<Availability[]>([]);
+	const [pagiantedData, setpagiantedData] =
+		useState<PaginateData<Availability[]>>();
+	const [pageNum, setPageNum] = useState(1);
 
-  const getDatesQuery = trpc.useQuery(["avalibility.getUserAvalibility"], {
-    onSuccess(data) {
-      console.log("this is the data: ", data);
-      setDates(data ?? []);
-    },
-  });
+	const getDatesQuery = trpc.useQuery(["avalibility.getUserAvalibility"], {
+		onSuccess(data) {
+			console.log("this is the data: ", data);
+			setDates(data ?? []);
+		},
+	});
 
-  useEffect(() => {
-    if (dates != undefined) {
-      const _paginated = paginate(dates, pageNum);
-      setpagiantedData(_paginated);
-      // setlocationPaginated(_paginated.data);
-    }
-  }, [dates, pageNum]);
+	useEffect(() => {
+		if (dates != undefined) {
+			const _paginated = paginate(dates, pageNum);
+			setpagiantedData(_paginated);
+			// setlocationPaginated(_paginated.data);
+		}
+	}, [dates, pageNum]);
 
-  const deleteDateMutation = trpc.useMutation("avalibility.deleteDate");
+	const deleteDateMutation = trpc.useMutation("avalibility.deleteDate", {
+		onSuccess(data) {
+			setDates(dates.filter((item) => item.id != data?.id));
+			getDatesQuery.refetch();
+		},
+	});
 
-  if (pagiantedData == undefined || getDatesQuery.isFetching) {
-    return (
-      <div className='flex justify-center'>
-        <CircularProgress />
-      </div>
-    );
-  }
-  return (
-    <>
-      <div className='mb-8 flex justify-between gap-4 md:hidden'>
-        <SectionHeading>Unavaliable Dates</SectionHeading>
-        <div>
-          <BtnAdd onClick={() => setModalOpen(true)} />
-          <LimitSelect selected={limit} setSelected={setLimit} />
-        </div>
-      </div>
-      <div className='mb-8 hidden justify-between md:flex'>
-        <SectionHeading>Unavailable Dates</SectionHeading>
-        <div>
-          <BtnAdd onClick={() => setModalOpen(true)} />
-          <LimitSelect selected={limit} setSelected={setLimit} />
-        </div>
-      </div>
+	if (pagiantedData == undefined || getDatesQuery.isLoading) {
+		return (
+			<div className="flex justify-center">
+				<CircularProgress />
+			</div>
+		);
+	}
+	return (
+		<>
+			<div className="mb-8 flex justify-between gap-4 md:hidden">
+				<SectionHeading>Unavaliable Dates</SectionHeading>
+				<div>
+					<BtnAdd onClick={() => setModalOpen(true)} />
+					<LimitSelect selected={limit} setSelected={setLimit} />
+				</div>
+			</div>
+			<div className="mb-8 hidden justify-between md:flex">
+				<SectionHeading>Unavailable Dates</SectionHeading>
+				<div>
+					<BtnAdd onClick={() => setModalOpen(true)} />
+					<LimitSelect selected={limit} setSelected={setLimit} />
+				</div>
+			</div>
 
-      <AvaililityModal
-        open={modalOpen}
-        setOpen={setModalOpen}
-        exisitingDates={dates}
-        setExisitingDates={setDates}
-      />
-      <div className='w-full'>
-        <table className='w-full table-auto text-left'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dates?.map((date, index) => {
-              const options: TableOptionsDropdown = [
-                {
-                  name: "Delete",
-                  function: () => {
-                    deleteDateMutation.mutate(date.id);
-                  },
-                },
-              ];
+			<AvaililityModal
+				open={modalOpen}
+				setOpen={setModalOpen}
+				exisitingDates={dates}
+				setExisitingDates={setDates}
+			/>
+			<div className="w-full">
+				<table className="w-full table-auto text-left">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{dates?.map((date, index) => {
+							const options: TableOptionsDropdown = [
+								{
+									name: "Delete",
+									function: () => {
+										deleteDateMutation.mutate(date.id);
+									},
+								},
+							];
 
-              return (
-                <tr key={index} className='border-t last:border-b'>
-                  <td className='py-4 text-base leading-4 text-gray-800 md:text-xl'>
-                    {Intl.DateTimeFormat("en-US", {
-                      weekday: "long",
-                      month: "long",
-                      day: "numeric",
-                      year: "numeric",
-                    }).format(date.date)}
-                  </td>
-                  <td className=''>
-                    <TableDropdown options={options} />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <PaginationBar
-        setPageNum={setPageNum}
-        pageNum={pageNum}
-        paginateData={pagiantedData}
-      />
-    </>
-  );
+							return (
+								<tr key={index} className="border-t last:border-b">
+									<td className="py-4 text-base leading-4 text-gray-800 md:text-xl">
+										{Intl.DateTimeFormat("en-US", {
+											weekday: "long",
+											month: "long",
+											day: "numeric",
+											year: "numeric",
+										}).format(date.date)}
+									</td>
+									<td className="">
+										<TableDropdown options={options} />
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+			<PaginationBar
+				setPageNum={setPageNum}
+				pageNum={pageNum}
+				paginateData={pagiantedData}
+			/>
+			<button className="bg-red-500">fdfd</button>
+		</>
+	);
 };
 
 AvailabilityPage.getLayout = sidebar;

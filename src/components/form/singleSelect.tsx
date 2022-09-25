927d50f4ae8adbaf5caeae53aa37d25486b33974
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2022-09-25 11:49:07 -0500

    new secletion component

--- src/components/form/singleSelect.tsx
+++ src/components/form/singleSelect.tsx
@@ -4,61 +4,128 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 //this requies data to have an id and name property
 export const SingleSelect: React.FC<{
-	selected: any;
-	setSelected: Dispatch<SetStateAction<any>>;
-	list: any[];
+  selected: any;
+  setSelected: Dispatch<SetStateAction<any>>;
+  list: any[];
 }> = ({ selected, setSelected, list }) => {
-	if (!list) return <div></div>;
+  if (!list) return <div></div>;
 
-	return (
-		<div className=" block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-			<Listbox value={selected} onChange={(value) => setSelected(value)}>
-				<div className="relative mt-1 ">
-					<Listbox.Button className="relative h-full w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-						<div className="flex min-h-[1.5rem] flex-wrap">
-							{selected.name || ""}
-						</div>
-						<div className="absolute right-1 top-1/2 -translate-y-1/2">
-							<MdOutlineKeyboardArrowDown size={20} className="text-gray-500" />
-						</div>
-					</Listbox.Button>
-					<Transition
-						as={Fragment}
-						leave="transition ease-in duration-100"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0">
-						<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-							{list.map((item, index) => {
-								if (item.show != false) {
-									return (
-										<Listbox.Option
-											key={index}
-											className={({ active }) =>
-												`relative cursor-default select-none py-2 pl-10 pr-4 ${
-													active ? "bg-indigo-100" : "text-gray-900"
-												}`
-											}
-											value={item}>
-											{({ selected }) => (
-												<>
-													<span
-														className={`block truncate ${
-															selected
-																? "font-medium text-indigo-700"
-																: "font-normal"
-														}`}>
-														{item.name}
-													</span>
-												</>
-											)}
-										</Listbox.Option>
-									);
-								}
-							})}
-						</Listbox.Options>
-					</Transition>
-				</div>
-			</Listbox>
-		</div>
-	);
+  return (
+    <div className=' block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'>
+      <Listbox value={selected} onChange={(value) => setSelected(value)}>
+        <div className='relative mt-1 '>
+          <Listbox.Button className='relative h-full w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
+            <div className='flex min-h-[1.5rem] flex-wrap'>
+              {selected.name || ""}
+            </div>
+            <div className='absolute right-1 top-1/2 -translate-y-1/2'>
+              <MdOutlineKeyboardArrowDown size={20} className='text-gray-500' />
+            </div>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'>
+            <Listbox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+              {list.map((item, index) => {
+                if (item.show != false) {
+                  return (
+                    <Listbox.Option
+                      key={index}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-indigo-100" : "text-gray-900"
+                        }`
+                      }
+                      value={item}>
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected
+                                ? "font-medium text-indigo-700"
+                                : "font-normal"
+                            }`}>
+                            {item.name}
+                          </span>
+                        </>
+                      )}
+                    </Listbox.Option>
+                  );
+                }
+              })}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+};
+
+interface SingleSelectProps<ListItem extends Object> {
+  selected: { item: ListItem; label: string };
+  setSelected: Dispatch<SetStateAction<{ item: ListItem; label: string }>>;
+  list: { item: ListItem; label: string; hide?: boolean }[];
+}
+
+//this requies data to have an id and name property
+export const NewSingleSelect = <List extends Object>({
+  selected,
+  setSelected,
+  list,
+}: SingleSelectProps<List>) => {
+  if (!list) return <div></div>;
+
+  return (
+    <div className=' block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'>
+      <Listbox value={selected} onChange={(value) => setSelected(value)}>
+        <div className='relative mt-1 '>
+          <Listbox.Button className='relative h-full w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
+            <div className='flex min-h-[1.5rem] flex-wrap'>
+              {selected.label || ""}
+            </div>
+            <div className='absolute right-1 top-1/2 -translate-y-1/2'>
+              <MdOutlineKeyboardArrowDown size={20} className='text-gray-500' />
+            </div>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'>
+            <Listbox.Options className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+              {list.map((item, index) => {
+                if (item.hide == true) {
+                  return (
+                    <Listbox.Option
+                      key={index}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-indigo-100" : "text-gray-900"
+                        }`
+                      }
+                      value={item}>
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected
+                                ? "font-medium text-indigo-700"
+                                : "font-normal"
+                            }`}>
+                            {item.label}
+                          </span>
+                        </>
+                      )}
+                    </Listbox.Option>
+                  );
+                }
+              })}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
 };

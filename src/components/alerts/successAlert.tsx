8fc97a1acf1ddaf50eec4684beb9e73a556cf62d
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2022-11-01 08:51:05 -0500

    success alert updated

--- src/components/alerts/successAlert.tsx
+++ src/components/alerts/successAlert.tsx
@@ -1,57 +1,51 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useContext, useState } from "react";
 import { MdOutlineCheckCircleOutline } from "react-icons/md";
+import { AlertContext } from "../../providers/alertProvider";
 
-export const SuccdssAlert: React.FC<{
-	error: { state: boolean; message: string };
-	setState: Dispatch<
-		SetStateAction<{
-			state: boolean;
-			message: string;
-		}>
-	>;
-}> = ({ error, setState }) => {
-	const [flag, setFlag] = useState(true);
+export const SuccdssAlert = () => {
+  const [flag, setFlag] = useState(true);
+  const context = useContext(AlertContext);
 
-	if (error.state == true) {
-		setTimeout(() => {
-			setState({ state: false, message: "" });
-		}, 3500);
-	}
-	return (
-		<div className="absolute right-0 top-0 z-50 text-green-600">
-			{/* Code block starts */}
+  if (context.success.state == true) {
+    setTimeout(() => {
+      context.setSuccess({ state: false, message: "" });
+    }, 3500);
+  }
+  return (
+    <div className='absolute right-0 top-0 z-50 text-green-600'>
+      {/* Code block starts */}
 
-			<div className="flex items-center justify-center px-4 sm:px-0 ">
-				<div
-					id="alert"
-					className={
-						flag
-							? "top-0 mt-12 mb-8 flex-col justify-between rounded-md border border-green-500 bg-green-200  py-4 px-4 shadow  transition duration-150 ease-in-out md:flex "
-							: "translate-hidden top-0 mt-12 mb-8 items-center justify-between rounded-md border border-green-500  bg-green-200 py-4 px-4  shadow transition duration-150 ease-in-out md:flex lg:w-10/12"
-					}>
-					<div className="flex items-center">
-						<div className="flex items-end">
-							<div className="mr-2 mt-0.5 text-green-600 sm:mt-0">
-								<MdOutlineCheckCircleOutline size={24} />
-							</div>
-							<p className="mr-2 text-base font-bold ">Success</p>
-						</div>
-						{/* <div className='mr-2 hidden  h-1 w-1 rounded-full xl:block' /> */}
-						<p className="text-base ">{error.message}</p>
-					</div>
-					<div className="mt-4 flex justify-end md:mt-0 md:pl-4">
-						<span
-							onClick={() => setState({ state: false, message: "" })}
-							className="cursor-pointer text-sm text-black">
-							Dismiss
-						</span>
-					</div>
-				</div>
-			</div>
+      <div className='flex items-center justify-center px-4 sm:px-0 '>
+        <div
+          id='alert'
+          className={
+            flag
+              ? "top-0 mt-12 mb-8 flex-col justify-between rounded-md border border-green-500 bg-green-200  py-4 px-4 shadow  transition duration-150 ease-in-out md:flex "
+              : "translate-hidden top-0 mt-12 mb-8 items-center justify-between rounded-md border border-green-500  bg-green-200 py-4 px-4  shadow transition duration-150 ease-in-out md:flex lg:w-10/12"
+          }>
+          <div className='flex items-center'>
+            <div className='flex items-end'>
+              <div className='mr-2 mt-0.5 text-green-600 sm:mt-0'>
+                <MdOutlineCheckCircleOutline size={24} />
+              </div>
+              <p className='mr-2 text-base font-bold '>Success</p>
+            </div>
+            {/* <div className='mr-2 hidden  h-1 w-1 rounded-full xl:block' /> */}
+            <p className='text-base '>{context.success.message}</p>
+          </div>
+          <div className='mt-4 flex justify-end md:mt-0 md:pl-4'>
+            <span
+              onClick={() => context.setSuccess({ state: false, message: "" })}
+              className='cursor-pointer text-sm text-black'>
+              Dismiss
+            </span>
+          </div>
+        </div>
+      </div>
 
-			{/* Code block ends */}
-			<style>
-				{`
+      {/* Code block ends */}
+      <style>
+        {`
               .translate-show{
                   transform : translateY(0%);
               }
@@ -59,7 +53,7 @@ export const SuccdssAlert: React.FC<{
                   transform : translateY(18vh);
               }
               `}
-			</style>
-		</div>
-	);
+      </style>
+    </div>
+  );
 };

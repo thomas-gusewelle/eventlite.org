4d295ae06b2f4b97502c1e1ef53a975977d1d3f7
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2022-12-30 15:55:42 -0600

    auto user response to false on new unavailable

--- src/server/router/availability.ts
+++ src/server/router/availability.ts
@@ -44,6 +44,44 @@ export const avalibiltyRouter = createRouter()
           date: date,
         })),
       });
+
+      // find dates user has responsed to and set the response to be DENY
+      if (input.newDates.length > 0) {
+        const schedule = await prisma?.eventPositions.findMany({
+          where: {
+            userId: input.userId,
+          },
+          select: {
+            id: true,
+            userResponse: true,
+            Event: {
+              select: {
+                datetime: true,
+              },
+            },
+          },
+        });
+
+        const changeDates = schedule?.filter((item) =>
+          input.newDates
+            .map((date) => date.getTime())
+            .includes(
+              new Date(item.Event.datetime.getTime()).setHours(0, 0, 0, 0)
+            )
+        );
+
+        await prisma?.eventPositions.updateMany({
+          where: {
+            id: {
+              in: changeDates?.map((item) => item.id),
+            },
+          },
+          data: {
+            userResponse: false,
+          },
+        });
+      }
+
       const deleteDates = await prisma?.availability.deleteMany({
         where: {
           userId: input.userId,

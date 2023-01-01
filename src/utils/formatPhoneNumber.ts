ad0406dd6a9cc441e0cc8b099add253f43d3cf03
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-01-01 17:23:45 -0600

    formatPhone helper function

---
+++ src/utils/formatPhoneNumber.ts
@@ -0,0 +1,12 @@
+export function formatPhoneNumber(e: string): string {
+  if (e.length == 3) {
+    return e + "-";
+  } else if (e.length >= 7) {
+    e = e.replace(/-/g, "");
+    let areaCode = e.slice(0, 3);
+    let nextThree = e.slice(3, 6);
+    let rest = e.slice(6);
+    return areaCode + "-" + nextThree + "-" + rest;
+  }
+  return e;
+}

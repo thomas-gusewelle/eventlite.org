f37cfeddc8efe8ede2ac19d2682b2d9ba50f4b6b
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-01-03 08:38:04 -0600

    already have account button

--- src/components/create-account-flow/stepCounter.tsx
+++ src/components/create-account-flow/stepCounter.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const StepCounter = ({
   signUpState,
   totalNum,
@@ -11,41 +13,30 @@ const StepCounter = ({
   }
 
   return (
-    <div className='mx-auto mb-4 flex items-center justify-center'>
-      {loop.map((item) => (
-        <>
-          <div
-            key={item}
-            className={`flex h-12 w-12 items-center justify-center rounded-full ${
-              signUpState === item
-                ? "border-[3px] bg-indigo-700 text-white"
-                : "bg-white text-stone-800"
-            }`}>
-            <span>{item}</span>
-          </div>
-          {item != totalNum && <div className='mx-2 h-1 w-12 bg-white'></div>}
-        </>
-      ))}
-
-      {/* <div className='mx-2 h-1 w-12 bg-white'></div>
-      <div
-        className={`flex h-12 w-12 items-center justify-center rounded-full ${
-          signUpState === 2
-            ? "bg-green-500 text-white"
-            : "bg-white text-stone-800"
-        }`}>
-        <span>2</span>
+    <>
+      <div className='mx-auto mb-4 flex items-center justify-center'>
+        {loop.map((item) => (
+          <>
+            <div
+              key={item}
+              className={`flex h-12 w-12 items-center justify-center rounded-full ${
+                signUpState === item
+                  ? "border-[3px] bg-indigo-700 text-white"
+                  : "bg-white text-stone-800"
+              }`}>
+              <span>{item}</span>
+            </div>
+            {item != totalNum && <div className='mx-2 h-1 w-12 bg-white'></div>}
+          </>
+        ))}
       </div>
-      
-      <div
-        className={`flex h-12 w-12 items-center justify-center rounded-full ${
-          signUpState === 3
-            ? "bg-green-500 text-white"
-            : "bg-white text-stone-800"
-        }`}>
-        <span>3</span>
-      </div> */}
-    </div>
+      <p className='text-center text-white'>
+        Already have an account?{" "}
+        <Link href={"/signin"}>
+          <a className='underline'>Sign In</a>
+        </Link>
+      </p>
+    </>
   );
 };
 

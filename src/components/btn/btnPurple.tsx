9453a26eeb521ac31308bdc90789a7733d2198ac
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-01-03 11:09:53 -0600

    type to btn purple

--- src/components/btn/btnPurple.tsx
+++ src/components/btn/btnPurple.tsx
@@ -1,12 +1,15 @@
 export const BtnPurple = ({
   children,
   func,
+  type = "button",
 }: {
   children: any;
   func?: () => void;
+  type?: "button" | "submit" | "reset";
 }) => {
   return (
     <button
+      type={type}
       tabIndex={1}
       className='flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
       onClick={func}>

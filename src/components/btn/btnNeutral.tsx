4fee1632503a883baa0a10421606be6fd5e3af35
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-01-03 10:50:49 -0600

    added optional function to neutral btn

--- src/components/btn/btnNeutral.tsx
+++ src/components/btn/btnNeutral.tsx
@@ -1,6 +1,13 @@
-export const BtnNeutral: React.FC<{ children: any }> = ({ children }) => {
+import { FormEvent, ReactNode } from "react";
+
+export const BtnNeutral: React.FC<{
+  children: ReactNode;
+  func?: () => void;
+}> = ({ children, func }) => {
   return (
-    <button className='200ms flex w-full items-center justify-center rounded-lg bg-gray-100 px-4 py-2 transition-all ease-in-out hover:bg-gray-200 sm:w-auto'>
+    <button
+      onClick={func}
+      className='200ms flex w-full items-center justify-center rounded-lg bg-gray-100 px-4 py-2 transition-all ease-in-out hover:bg-gray-200 sm:w-auto'>
       {children}
     </button>
   );

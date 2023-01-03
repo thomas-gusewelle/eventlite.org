5b0bf90762cbb9c6ef382a6dcfbae0b242c7e187
Thomas <tgusewelle@gkwmedia.com>
2023-01-03 15:28:07 -0600

    org creation in progress

--- src/components/create-account-flow/steps/createOrganization.tsx
+++ src/components/create-account-flow/steps/createOrganization.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { useFormContext } from "react-hook-form";
 import { CardHeader } from "../components/cardHeader";
 
 export const CreateOrganization = ({
@@ -6,9 +7,19 @@ export const CreateOrganization = ({
 }: {
   setStep: Dispatch<SetStateAction<number>>;
 }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   return (
     <>
       <CardHeader>Create Organization</CardHeader>
+      <section>
+        <div className='mt-3'>
+          <label className='form-label'>Organization Name</label>
+        </div>
+        <input type='text' className='input-field' {...register("orgName")} />
+      </section>
     </>
   );
 };

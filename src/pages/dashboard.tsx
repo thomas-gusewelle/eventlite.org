cda84142bbb1d87c2b12a17f9a49076b968145ce
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-01-15 11:51:11 -0600

    moving to needs aproval WIP

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -15,6 +15,8 @@ import { DashboardAvaililityModal } from "../components/modal/dashboard/availibi
 import { TableDropdown } from "../components/menus/tableDropdown";
 import { CircularProgress } from "../components/circularProgress";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
+import { BtnNeutral } from "../components/btn/btnNeutral";
+import { useRouter } from "next/router";
 
 type stateData = (Event & {
   Locations: Locations | null;
@@ -29,21 +31,29 @@ type stateData = (Event & {
 const Dashboard = () => {
   const user = useContext(UserContext);
   const utils = trpc.useContext();
+  const router = useRouter();
   const alertContext = useContext(AlertContext);
   const [availabilityModal, setAvailabilityModal] = useState(false);
   const [eventsData, setEventsData] = useState<stateData>([]);
+  const [approvalEventsData, setApprovalEventsData] = useState<stateData>([]);
+  const [page, setPage] = useState(1);
   // render, setRender is being used to force rerending of nested eventsData on mutate
   const [render, setRender] = useState(true);
 
   // const tempDelete = trpc.useMutation("organization.deleteOrg");
 
   const eventsQuery = trpc.useQuery(
-    ["events.getUpcomingEventsByUser", { page: 1 }],
+    ["events.getUpcomingEventsByUser", { page: page }],
 
     {
       onSuccess(data) {
         if (data != undefined) {
-          setEventsData(data);
+          if (data.upcoming != undefined) {
+            setEventsData(data.upcoming);
+          }
+          if (data.needApproval != undefined) {
+            setApprovalEventsData(data.needApproval);
+          }
         }
       },
       onError(err) {
@@ -62,6 +72,7 @@ const Dashboard = () => {
       });
       eventsQuery.refetch();
     },
+    // TODO make this work with moving from needs approval to upcoming
     onMutate(variables) {
       if (variables.positionId == undefined) return;
       // returns the event item we are working with
@@ -106,6 +117,14 @@ const Dashboard = () => {
     },
   });
 
+  if (eventsQuery.isLoading) {
+    return (
+      <div className='my-6 flex justify-center'>
+        <CircularProgress />
+      </div>
+    );
+  }
+
   return (
     <>
       <DashboardAvaililityModal
@@ -127,125 +146,235 @@ const Dashboard = () => {
             update Id
           </BtnPurple> */}
       </div>
+
+      {approvalEventsData.length > 0 && (
+        <>
+          {" "}
+          <div className='mt-8 flex justify-center'>
+            <SectionHeading>Needs Approval</SectionHeading>
+          </div>
+          <div className='my-6 grid grid-cols-1 gap-6 xl:grid-cols-2 2xl:grid-cols-3'>
+            {approvalEventsData.map((event) => {
+              let userResponse = event.positions.find(
+                (item) => item.userId == user?.id
+              );
+              return (
+                <div
+                  key={event.id}
+                  className='flex flex-col border-gray-300 pt-4'>
+                  <div
+                    className={`rounded-t-lg pt-2 ${
+                      userResponse?.userResponse == null
+                        ? ""
+                        : "rounded-b-lg border-b"
+                    } round border-t border-r border-l shadow`}>
+                    <div className='mb-4 flex flex-col px-3'>
+                      <div className='flex justify-between'>
+                        <h3 className='text-xl font-bold'>{event.name}</h3>
+                        {userResponse?.userResponse != null && (
+                          <TableDropdown
+                            options={[
+                              {
+                                name: `Change Availability to ${
+                                  userResponse.userResponse == true
+                                    ? "False"
+                                    : "True"
+                                }`,
+                                function: () =>
+                                  userResponseMutation.mutate({
+                                    positionId: userResponse?.id,
+                                    response:
+                                      userResponse?.userResponse == true
+                                        ? "DENY"
+                                        : "APPROVE",
+                                  }),
+                              },
+                              {
+                                name: "Change Availability to Unknown",
+                                function: () =>
+                                  userResponseMutation.mutate({
+                                    positionId: userResponse?.id,
+                                    response: "NULL",
+                                  }),
+                              },
+                            ]}
+                          />
+                        )}
+                      </div>
+
+                      <span className='text-lg font-medium'>
+                        {event.Locations?.name}
+                      </span>
+                      <span>{shortDate(event.datetime)}</span>
+                      <span>{shortTime(event.datetime)}</span>
+                    </div>
+                    <div className=''>
+                      {event.positions.map((position) => {
+                        return (
+                          <div
+                            className='grid grid-cols-[1fr_1.5fr] items-center border-t last:border-b last:pb-0'
+                            key={position.id}>
+                            <span className='py-3 px-3 font-medium'>
+                              {position.Role.name}
+                            </span>
+                            {position.User ? (
+                              <div
+                                className={`flex h-full py-1 px-3 text-center ${
+                                  position.userResponse == null && "bg-gray-100"
+                                }
+                    ${position.userResponse == true && "bg-green-200"}
+                    ${position.userResponse == false && "bg-red-200"}
+                    `}>
+                                <PicNameRowSmall user={position?.User} />
+                              </div>
+                            ) : (
+                              <div className='flex h-full items-center justify-center bg-gray-100 py-3 px-6 text-center leading-4'>
+                                Not Scheduled
+                              </div>
+                            )}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  {/* User approval section */}
+                  {userResponse?.userResponse == null && (
+                    <div className='grid grid-cols-2 overflow-hidden rounded-b-lg border-b border-r border-l'>
+                      <BtnApprove
+                        func={() =>
+                          userResponseMutation.mutate({
+                            response: "APPROVE",
+                            positionId: userResponse?.id,
+                          })
+                        }
+                      />
+                      <BtnDeny
+                        func={() =>
+                          userResponseMutation.mutate({
+                            response: "DENY",
+                            positionId: userResponse?.id,
+                          })
+                        }
+                      />
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
       <div className='mt-8 flex justify-center'>
         <SectionHeading>Upcoming Events</SectionHeading>
       </div>
 
-      {eventsQuery.isLoading || eventsQuery.isRefetching ? (
-        <div className='my-6 flex justify-center'>
-          <CircularProgress />
-        </div>
-      ) : (
-        <div className='my-6 grid grid-cols-1 gap-6 xl:grid-cols-2 2xl:grid-cols-3'>
-          {eventsData.map((event) => {
-            let userResponse = event.positions.find(
-              (item) => item.userId == user?.id
-            );
-            return (
+      <div className='my-6 grid grid-cols-1 gap-6 xl:grid-cols-2 2xl:grid-cols-3'>
+        {eventsData.map((event) => {
+          let userResponse = event.positions.find(
+            (item) => item.userId == user?.id
+          );
+          return (
+            <div key={event.id} className='flex flex-col border-gray-300 pt-4'>
               <div
-                key={event.id}
-                className='flex flex-col border-gray-300 pt-4'>
-                <div
-                  className={`rounded-t-lg pt-2 ${
-                    userResponse?.userResponse == null
-                      ? ""
-                      : "rounded-b-lg border-b"
-                  } round border-t border-r border-l shadow`}>
-                  <div className='mb-4 flex flex-col px-3'>
-                    <div className='flex justify-between'>
-                      <h3 className='text-xl font-bold'>{event.name}</h3>
-                      {userResponse?.userResponse != null && (
-                        <TableDropdown
-                          options={[
-                            {
-                              name: `Change Availability to ${
-                                userResponse.userResponse == true
-                                  ? "False"
-                                  : "True"
-                              }`,
-                              function: () =>
-                                userResponseMutation.mutate({
-                                  positionId: userResponse?.id,
-                                  response:
-                                    userResponse?.userResponse == true
-                                      ? "DENY"
-                                      : "APPROVE",
-                                }),
-                            },
-                            {
-                              name: "Change Availability to Unknown",
-                              function: () =>
-                                userResponseMutation.mutate({
-                                  positionId: userResponse?.id,
-                                  response: "NULL",
-                                }),
-                            },
-                          ]}
-                        />
-                      )}
-                    </div>
-
-                    <span className='text-lg font-medium'>
-                      {event.Locations?.name}
-                    </span>
-                    <span>{shortDate(event.datetime)}</span>
-                    <span>{shortTime(event.datetime)}</span>
+                className={`rounded-t-lg pt-2 ${
+                  userResponse?.userResponse == null
+                    ? ""
+                    : "rounded-b-lg border-b"
+                } round border-t border-r border-l shadow`}>
+                <div className='mb-4 flex flex-col px-3'>
+                  <div className='flex justify-between'>
+                    <h3 className='text-xl font-bold'>{event.name}</h3>
+                    {userResponse?.userResponse != null && (
+                      <TableDropdown
+                        options={[
+                          {
+                            name: `Change Availability to ${
+                              userResponse.userResponse == true
+                                ? "False"
+                                : "True"
+                            }`,
+                            function: () =>
+                              userResponseMutation.mutate({
+                                positionId: userResponse?.id,
+                                response:
+                                  userResponse?.userResponse == true
+                                    ? "DENY"
+                                    : "APPROVE",
+                              }),
+                          },
+                          {
+                            name: "Change Availability to Unknown",
+                            function: () =>
+                              userResponseMutation.mutate({
+                                positionId: userResponse?.id,
+                                response: "NULL",
+                              }),
+                          },
+                        ]}
+                      />
+                    )}
                   </div>
-                  <div className=''>
-                    {event.positions.map((position) => {
-                      return (
-                        <div
-                          className='grid grid-cols-[1fr_1.5fr] items-center border-t last:border-b last:pb-0'
-                          key={position.id}>
-                          <span className='py-3 px-3 font-medium'>
-                            {position.Role.name}
-                          </span>
-                          {position.User ? (
-                            <div
-                              className={`flex h-full py-1 px-3 text-center ${
-                                position.userResponse == null && "bg-gray-100"
-                              }
+
+                  <span className='text-lg font-medium'>
+                    {event.Locations?.name}
+                  </span>
+                  <span>{shortDate(event.datetime)}</span>
+                  <span>{shortTime(event.datetime)}</span>
+                </div>
+                <div className=''>
+                  {event.positions.map((position) => {
+                    return (
+                      <div
+                        className='grid grid-cols-[1fr_1.5fr] items-center border-t last:border-b last:pb-0'
+                        key={position.id}>
+                        <span className='py-3 px-3 font-medium'>
+                          {position.Role.name}
+                        </span>
+                        {position.User ? (
+                          <div
+                            className={`flex h-full py-1 px-3 text-center ${
+                              position.userResponse == null && "bg-gray-100"
+                            }
                     ${position.userResponse == true && "bg-green-200"}
                     ${position.userResponse == false && "bg-red-200"}
                     `}>
-                              <PicNameRowSmall user={position?.User} />
-                            </div>
-                          ) : (
-                            <div className='flex h-full items-center justify-center bg-gray-100 py-3 px-6 text-center leading-4'>
-                              Not Scheduled
-                            </div>
-                          )}
-                        </div>
-                      );
-                    })}
-                  </div>
+                            <PicNameRowSmall user={position?.User} />
+                          </div>
+                        ) : (
+                          <div className='flex h-full items-center justify-center bg-gray-100 py-3 px-6 text-center leading-4'>
+                            Not Scheduled
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
                 </div>
-                {/* User approval section */}
-                {userResponse?.userResponse == null && (
-                  <div className='grid grid-cols-2 overflow-hidden rounded-b-lg border-b border-r border-l'>
-                    <BtnApprove
-                      func={() =>
-                        userResponseMutation.mutate({
-                          response: "APPROVE",
-                          positionId: userResponse?.id,
-                        })
-                      }
-                    />
-                    <BtnDeny
-                      func={() =>
-                        userResponseMutation.mutate({
-                          response: "DENY",
-                          positionId: userResponse?.id,
-                        })
-                      }
-                    />
-                  </div>
-                )}
               </div>
-            );
-          })}
-        </div>
-      )}
+              {/* User approval section */}
+              {userResponse?.userResponse == null && (
+                <div className='grid grid-cols-2 overflow-hidden rounded-b-lg border-b border-r border-l'>
+                  <BtnApprove
+                    func={() =>
+                      userResponseMutation.mutate({
+                        response: "APPROVE",
+                        positionId: userResponse?.id,
+                      })
+                    }
+                  />
+                  <BtnDeny
+                    func={() =>
+                      userResponseMutation.mutate({
+                        response: "DENY",
+                        positionId: userResponse?.id,
+                      })
+                    }
+                  />
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };

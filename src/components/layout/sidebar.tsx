0ce7160b1a9097e9f70216318d91684e34e0ba1a
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-01-16 20:36:42 -0600

    moved mobile sidebar menu

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -269,7 +269,7 @@ export const SidebarLayout: React.FC<{ children: any }> = ({ children }) => {
                           leave='transition ease-in duration-75'
                           leaveFrom='transform opacity-100 scale-100'
                           leaveTo='transform opacity-0 scale-95'>
-                          <Menu.Items className='absolute left-[125%] bottom-0 z-50 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                          <Menu.Items className='absolute right-0 bottom-[150%] z-50 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                             <div className='py-1'>
                               <Menu.Item key={0}>
                                 {({ active }) => (

aa1ea9a03cbc29817ad7e889512dead653ea428c
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-02-17 15:30:49 -0600

    wip

--- src/emails/ConfirmEmailNew.tsx
+++ src/emails/ConfirmEmailNew.tsx
@@ -5,19 +5,50 @@ import Heading from "./components/Heading";
 import Text from "./components/Text";
 import Button from "./components/Button";
 import { MjmlWrapper, MjmlColumn, MjmlSection } from "mjml-react";
-import { colors, fontFamily } from "./theme";
+import { colors, fontFamily, fontSize, screens } from "./theme";
+
+const styles = `
+  .h1 > * {
+    font-size: 48px !important;
+  }
+  .h2 > * {
+    font-size: ${fontSize.lg}px !important;
+  }
+  .p > * {
+    font-size: ${fontSize.base}px !important;
+  }
+  .mt-6 > * {
+    padding-top: 12px !important;
+  }
+
+  @media (min-width:${screens.xs}) {
+    .h1 > * {
+      font-size: 42px !important;
+    }
+    .h2 > * {
+      font-size: ${fontSize.xxl}px !important;
+    }
+    .p > * {
+      font-size: ${fontSize.md}px !important;
+    }
+  }
+`;
 
 const ConfirmEmail = () => {
   return (
-    <BaseLayout>
+    <BaseLayout style={styles}>
       <Header />
       <MjmlWrapper>
-        <MjmlSection>
+        <MjmlSection cssClass='gutter'>
           <MjmlColumn>
-            <Heading maxWidth={420} color={colors.black000}>
-              Please Confirm Your Email
+            <Heading cssClass='h1' color={colors.black000}>
+              Confirm Your Email Address
             </Heading>
-            <Text color={colors.black}>
+          </MjmlColumn>
+        </MjmlSection>
+        <MjmlSection cssClass='gutter'>
+          <MjmlColumn>
+            <Text color={colors.black} paddingTop={24} paddingBottom={24}>
               Confirming your email allows us and your organization to know
               it&apos;s really you. Please use the button below to confirm your
               email.

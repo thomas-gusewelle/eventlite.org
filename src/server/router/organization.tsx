170db8086e6ad696c648bbdc3a74b5d59fb62573
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-03-15 17:33:27 -0500

    org router

--- src/server/router/organization.tsx
+++ src/server/router/organization.tsx
@@ -4,11 +4,12 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import sendMail from "../../emails";
 import ConfirmEmailNew from "../../emails/accounts/ConfirmEmailNew";
-import { createRouter } from "./context";
+import { createTRPCRouter, publicProcedure, adminProcedure } from "./context";
 
-export const organizationRouter = createRouter()
-  .mutation("createOrg", {
-    input: z.object({
+
+export const organizationRouter = createTRPCRouter({
+  createOrg: publicProcedure.input(
+    z.object({
       firstName: z.string(),
       lastName: z.string(),
       email: z.string().email(),
@@ -22,81 +23,72 @@ export const organizationRouter = createRouter()
       orgName: z.string(),
       orgPhoneNumber: z.string().optional(),
     }),
-    async resolve({ input }) {
+  ).mutation(async ({ ctx, input }) => {
 
-      // Create signup link and send to user
-      const _supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.SUPABASE_PRIVATE!
-      );
 
-      const { data, error } = await _supabase.auth.admin.generateLink({
-        email: input.email,
-        type: "signup",
-        password: input.password,
+    // Create signup link and send to user
+    const _supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.SUPABASE_PRIVATE!
+    );
+
+    const { data, error } = await _supabase.auth.admin.generateLink({
+      email: input.email,
+      type: "signup",
+      password: input.password,
+    });
+    if (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error.message,
+        cause: "User Login Creation",
       });
-      if (error) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: error.message,
-          cause: "User Login Creation",
-        });
-      }
+    }
 
-      try {
-        await sendMail({
-          to: input.email,
-          component: <ConfirmEmailNew link={data.properties.action_link} />,
-        });
-      } catch (error) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Email send failed",
-        });
-      }
-
-      //Create organization and user in org (not the login)
-      const org = await prisma?.organization.create({
-        data: {
-          name: input.orgName,
-          phone_number: input.orgPhoneNumber,
-        },
+    try {
+      await sendMail({
+        to: input.email,
+        component: <ConfirmEmailNew link={data.properties.action_link} />,
       });
-
-      const user = await prisma?.user.create({
-        data: {
-          id: data.user.id,
-          firstName: input.firstName,
-          lastName: input.lastName,
-          email: input.email,
-          phoneNumber: input.phoneNumber,
-          organizationId: org?.id,
-          hasLogin: true,
-          status: input.status as UserStatus,
-          UserSettings: {
-            create: {},
-          },
-        },
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Email send failed",
       });
+    }
 
-      try {
-        await sendMail({ to: "tgusewelle@eventlite.org", subject: "Organization Created", text: `Awesome! Someone set up an organization. The name was ${org?.name}` })
-      } catch (err) {
-
-      }
-
-      return { org: org, user: user };
-    },
-  })
-  .middleware(async ({ ctx, next }) => {
-    const user = await prisma?.user.findFirst({
-      where: { id: ctx.data?.user?.id },
-      select: {
-        status: true,
+    //Create organization and user in org (not the login)
+    const org = await prisma?.organization.create({
+      data: {
+        name: input.orgName,
+        phone_number: input.orgPhoneNumber,
       },
     });
-    if (user?.status != "ADMIN") {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const user = await prisma?.user.create({
+      data: {
+        id: data.user.id,
+        firstName: input.firstName,
+        lastName: input.lastName,
+        email: input.email,
+        phoneNumber: input.phoneNumber,
+        organizationId: org?.id,
+        hasLogin: true,
+        status: input.status as UserStatus,
+        UserSettings: {
+          create: {},
+        },
+      },
+    });
+
+    try {
+      await sendMail({ to: "tgusewelle@eventlite.org", subject: "Organization Created", text: `Awesome! Someone set up an organization. The name was ${org?.name}` })
+    } catch (err) {
+
     }
-    return next();
-  });
+
+    return { org: org, user: user };
+  })
+})
+
+

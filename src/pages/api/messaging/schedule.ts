06036f2aa31e718213e313157ee014dae1081809
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-03-20 15:20:34 -0500

    redo schedule

--- src/pages/api/messaging/schedule.ts
+++ src/pages/api/messaging/schedule.ts
@@ -73,14 +73,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 
 
-  // const sentEmails = await Promise.all(
-  //   emails.map(async email => {
-  //     return await qstashClient.publishJSON({
-  //       url: `https://${req.headers.host}/api/messaging/remindScheduleEmail`,
-  //       body: superjson.stringify(email),
-  //     })
-  //   })
-  // )
+  const sentEmails = await Promise.all(
+    emails.map(async email => {
+      return await qstashClient.publishJSON({
+        url: `https://${req.headers.host}/api/messaging/remindScheduleEmail`,
+        body: superjson.stringify(email),
+      })
+    })
+  )
 
   // res.status(201).json({
   //   today: today.toDateString(), tomorrow: tomorrow.toDateString(),
@@ -88,5 +88,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   //   events: events?.map(item => ({ name: item.name, date: item.datetime.toDateString() })),
   //   emails: emails.map(item => ({ name: item.user.firstName, events: item.events?.map(ev => ev.name) }))
   // })
-  res.status(201).send(superjson.stringify(emails[0]))
+  res.status(201).send(sentEmails)
 }

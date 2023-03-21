bea707bfca257a9c2532337fd75e68f38c5f05cd
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-03-21 11:38:19 -0500

    add timezone to email

--- src/components/dateTime/dates.tsx
+++ src/components/dateTime/dates.tsx
@@ -1,18 +1,20 @@
 // Formats date in MM/D/Y format
-export function shortDate(date: Date) {
+export function shortDate(date: Date, timeZone?: string) {
   return Intl.DateTimeFormat("en-US", {
     month: "2-digit",
     day: "numeric",
     year: "numeric",
+    timeZone: timeZone
   }).format(date);
 }
 
 // Formats date in Sunday, November 6, 2022 format
-export function longDate(date: Date) {
+export function longDate(date: Date, timeZone?: string) {
   return Intl.DateTimeFormat("en-US", {
     weekday: "long",
     month: "long",
     day: "numeric",
     year: "numeric",
+    timeZone: timeZone
   }).format(date);
 }
--- src/emails/schedule/dayBeforeEmail.tsx
+++ src/emails/schedule/dayBeforeEmail.tsx
@@ -78,7 +78,7 @@ const DayBeforeEmail = ({ data }: { data: ReminderEmailData }) => {
                 <Text color={colors.black} fontSize={fontSize.lg} fontWeight={fontWeight.bold}>{event?.name}</Text>
                 <Text color={colors.black} fontSize={fontSize.md}>{event?.Locations?.name}</Text>
                 <Text color={colors.black}>{shortDate(event.datetime)}</Text>
-                <Text color={colors.black}>{shortTime(event.datetime)}</Text>
+                <Text color={colors.black}>{shortTime(event.datetime, event.timezone)}</Text>
               </MjmlColumn>
             </MjmlSection>
             <MjmlSection border={"1px solid rgb(209 213 219)"} borderTop={"none"} borderRadius={"2rem"}>

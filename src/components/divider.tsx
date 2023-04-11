21ff715ef371af71fe1d24e95f2154204de88d95
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-04-11 10:05:42 -0500

    divier style

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,3 +1,3 @@
 export const Divider = () => {
-  return <div className="w-full h-[.1rem] my-1 rounded-lg bg-gray-300"></div>
+  return <div className="w-full h-[.1rem] mt-6 mb-6 rounded-lg bg-gray-300"></div>
 }

a49cb244c70c210f8cc3f91079772999db9eec5b
Thomas <tgusewelle@gkwmedia.com>
2023-04-12 14:56:07 -0500

    password change modal

--- src/components/modal/userSettings/passwordChange.tsx
+++ src/components/modal/userSettings/passwordChange.tsx
@@ -4,33 +4,45 @@ import { AlertContext } from "../../../providers/alertProvider"
 import { api } from "../../../server/utils/api"
 import { BtnNeutral } from "../../btn/btnNeutral"
 import { BtnPurple } from "../../btn/btnPurple"
-import { EmailInput } from "../../form/emailInput"
+import { PasswordField } from "../../form/password"
 import { BottomButtons } from "../bottomButtons"
 import { Modal } from "../modal"
 import { ModalBody } from "../modalBody"
 import { ModalTitle } from "../modalTitle"
 
 export const PasswordChange = ({ open, setOpen }: { open: boolean, setOpen: Dispatch<SetStateAction<boolean>> }) => {
-  const methods = useForm<{ password: string, confirmPassword: string }>()
+  const methods = useForm<{
+    password: string, passwordConfirm: string
+  }>()
   const { setError, setSuccess } = useContext((AlertContext))
   const changePasswordMutation = api.userSettings.changePassword.useMutation()
   const submit = methods.handleSubmit((data) => {
-    if (data.password != data.confirmPassword) {
-      methods.setError("confirmPassword", { message: "Passwords do not match" })
+    if (data.password != data.passwordConfirm) {
+      methods.setError("passwordConfirm", { message: "Passwords do not match" })
       return
     }
+
+    changePasswordMutation.mutate({ password: data.password, confirmPassword: data.passwordConfirm }, {
+      onError(err) {
+        setError({ state: true, message: err.message })
+        setOpen(false)
+      },
+      onSuccess() {
+        setSuccess({ state: true, message: "Password changed successfully" })
+        setOpen(false)
+      }
+    })
 
   })
   return (
     <FormProvider {...methods}>
       <form>
-
         <Modal open={open} setOpen={setOpen}>
           <ModalBody>
             <ModalTitle text={"Change email address"} />
             <div className='py-3'>
-              <EmailInput />
-              <EmailInput isConfirm />
+              <PasswordField />
+              <PasswordField isConfirm />
             </div>
           </ModalBody>
           <BottomButtons>

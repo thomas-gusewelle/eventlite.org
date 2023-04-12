52e289d82d04a60c1f5d30cfb304e6a3e2181b55
Thomas <tgusewelle@gkwmedia.com>
2023-04-12 14:56:46 -0500

    settings page wi[

--- src/pages/account/settings.tsx
+++ src/pages/account/settings.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { BtnPurple } from "../../components/btn/btnPurple";
 import { SliderBtn } from "../../components/btn/SliderBtn";
@@ -6,15 +6,39 @@ import { Divider } from "../../components/divider";
 import { SectionHeading } from "../../components/headers/SectionHeading";
 import { sidebar } from "../../components/layout/sidebar";
 import { EmailChange } from "../../components/modal/userSettings/emailChange";
+import { PasswordChange } from "../../components/modal/userSettings/passwordChange";
+import { AlertContext } from "../../providers/alertProvider";
 import { UserContext } from "../../providers/userProvider";
+import { api } from "../../server/utils/api";
 
 const UserSettingsPage = () => {
   const user = useContext(UserContext);
+  console.log(user);
+
   if (user == undefined || user == null) return
-  const [hidePhoneNum, setHidePhoneNum] = useState(user.UserSettings?.hidePhoneNum ?? false)
+  const context = api.useContext()
+  const { setError, setSuccess } = useContext(AlertContext)
+  // const [hidePhoneNum, setHidePhoneNum] = useState(user.UserSettings?.hidePhoneNum ?? false)
   const [openEditEmail, setOpenEditEmail] = useState(false)
-  const [reminderEmails, setReminderEmails] = useState(user.UserSettings?.sendReminderEmail ?? true)
+  // const [reminderEmails, setReminderEmails] = useState(user.UserSettings?.sendReminderEmail ?? true)
   const [openEditPassword, setOpenEditPassword] = useState(false)
+  const hidePhoneNum = useRef(user.UserSettings?.hidePhoneNum ?? false)
+  const reminderEmails = useRef(user.UserSettings?.sendReminderEmail ?? true)
+  const changePhoneNumMutation = api.userSettings.changeHidePhoneNum.useMutation(
+    {
+      onError(err) {
+        hidePhoneNum.current = user.UserSettings?.hidePhoneNum ?? false
+        reminderEmails.current = user.UserSettings?.sendReminderEmail ?? true
+        setError({ state: true, message: err.message })
+      },
+      onSuccess() {
+        context.user.getUser.invalidate()
+        setSuccess({ state: true, message: "Setting successfully saved" })
+      }
+    }
+  )
+  // const settingsMutation = () => { changePhoneNumMutation.mutate({ hidePhoneNum: hidePhoneNum, sendReminderEmail: reminderEmails }) }
+  // const changeReminderEmailsMutation = api.userSettings.changeRemidnerEmails.useMutation()
   return (
     <div className="max-w-[960px] mx-auto">
       <section>
@@ -26,14 +50,15 @@ const UserSettingsPage = () => {
             <BtnPurple onClick={() => { setOpenEditEmail(true) }}>Edit</BtnPurple>
           </div>
         </div>
-        {createPortal(<EmailChange open={openEditEmail} setOpen={setOpenEditEmail} />, document.body)}
+        {openEditEmail && createPortal(<EmailChange open={openEditEmail} setOpen={setOpenEditEmail} />, document.body)}
         <div className="mt-3 flex justify-between items-center">
           <h3 className="text-xl font-bold">Password</h3>
           <p>*********</p>
           <div className="w-min">
-            <BtnPurple>Edit</BtnPurple>
+            <BtnPurple onClick={() => setOpenEditPassword(true)}>Edit</BtnPurple>
           </div>
         </div>
+        {openEditPassword && createPortal(<PasswordChange open={openEditPassword} setOpen={setOpenEditPassword} />, document.body)}
 
         <Divider />
       </section>
@@ -41,7 +66,9 @@ const UserSettingsPage = () => {
         <SectionHeading>Privacy</SectionHeading>
         <div className="flex items-center justify-between mt-3">
           <h3 className="text-xl font-bold">Hide Phone Number</h3>
-          <SliderBtn isChecked={hidePhoneNum} setIsChecked={setHidePhoneNum} />
+          <SliderBtn mutation={() =>
+            changePhoneNumMutation.mutate({ sendReminderEmail: reminderEmails.current, hidePhoneNum: hidePhoneNum.current })
+          } isChecked={hidePhoneNum} />
         </div>
         <Divider />
       </section>
@@ -49,7 +76,9 @@ const UserSettingsPage = () => {
         <SectionHeading>Notifications</SectionHeading>
         <div className="flex items-center justify-between mt-3">
           <h3 className="text-xl font-bold">Reminder Emails</h3>
-          <SliderBtn isChecked={reminderEmails} setIsChecked={setReminderEmails} />
+          <SliderBtn mutation={() =>
+            changePhoneNumMutation.mutate({ sendReminderEmail: reminderEmails.current, hidePhoneNum: hidePhoneNum.current })
+          } isChecked={reminderEmails} />
         </div>
       </section>
     </div>

503fe52b84ab2e7fec87cdc49c6fbe31297d390a
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2024-01-11 13:35:11 -0600

    datastore should be done

--- src/components/create-account-flow/dataStore.tsx
+++ src/components/create-account-flow/dataStore.tsx
@@ -1,17 +1,63 @@
-import { Context, createContext, Dispatch, SetStateAction, useContext, useState } from "react";
+import { CreateRouterInner } from "@trpc/server";
+import {
+  Context,
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
-export const CreateOrgContext = createContext<{state: any, setState: Dispatch<SetStateAction<any>>} | undefined>(undefined);
+type CreateAccountForm = {
+  orgID?: string;
+  orgName: string;
+  orgPhoneNumber?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+};
+export const CreateOrgContext = createContext<CreateAccountForm>({
+  orgID: undefined,
+  orgName: "",
+  orgPhoneNumber: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  confirmPassword: "",
+});
 
-export function CreateOrgProvider({ children }: {children: any}) {
-  const [state, setState] = useState({});
-  
+export function CreateOrgProvider({ children }: { children: any }) {
+const value = {
+  orgID: undefined,
+  orgName: "",
+  orgPhoneNumber: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  confirmPassword: "",
+}
+  useEffect(() => {
+    console.log(value);
+  }, [value])
   return (
-  <CreateOrgContext.Provider value={{state, setState}}></CreateOrgContext.Provider>
+    <CreateOrgContext.Provider
+      value={value}
+    ></CreateOrgContext.Provider>
   );
 }
 
 export function useAppState() {
   const context = useContext(CreateOrgContext);
+
+  //TODO: use this info to break each form into individual form allowing for me to not have to do custom validation.
   if (!context) {
     throw new Error("useAppState must be used within the AppProvider");
   }

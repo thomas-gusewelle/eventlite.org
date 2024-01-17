b7ee780d769246221cf27c614a2da1ef63d9c7b1
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2024-01-16 21:22:00 -0600

    change env

--- src/server/stripe/client.ts
+++ src/server/stripe/client.ts
@@ -1,3 +1,6 @@
+import { loadStripe } from "@stripe/stripe-js";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC!);
+
+export const loadedStripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC!);

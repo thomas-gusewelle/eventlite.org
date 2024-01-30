fba2c15ebe6010047128e599fbc05f96c9bc2e9b
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2024-01-30 11:57:17 -0600

    card info showing

--- src/components/create-account-flow/steps/cardInfo.tsx
+++ src/components/create-account-flow/steps/cardInfo.tsx
@@ -1,20 +1,43 @@
 import { Elements, PaymentElement } from "@stripe/react-stripe-js";
+import { Dispatch, SetStateAction } from "react";
 import { loadedStripe } from "../../../server/stripe/client";
+import { api } from "../../../server/utils/api";
+import { BtnNeutral } from "../../btn/btnNeutral";
+import { BtnPurple } from "../../btn/btnPurple";
+import { CardHeader } from "../components/cardHeader";
 export const CardInfoSection = ({
-  stripeClientSecret,
+  setStep,
+  stripeCustomerId
 }: {
-  stripeClientSecret: string | null;
+  setStep: Dispatch<SetStateAction<number>>
+  stripeCustomerId: string
 }) => {
-  if (stripeClientSecret == null) {
-    return <></>;
-  }
+
+  const clientSecret = api.stripe.createSetupIntent.useQuery({customerId: stripeCustomerId})
 
   return (
-    <Elements
-      stripe={loadedStripe}
-      options={{ clientSecret: stripeClientSecret }}
-    >
-      <PaymentElement />
-    </Elements>
+    <>
+      <CardHeader>Setup your payment method</CardHeader>
+      <Elements
+        stripe={loadedStripe}
+        options={{ mode: "setup", currency: "usd" }}
+      >
+        <PaymentElement />
+      </Elements>
+      <div className="mt-6 flex items-center justify-center gap-6">
+        <BtnNeutral
+          fullWidth
+          func={() => {
+            console.log("back being called");
+            setStep(3)
+          }}
+        >
+          Back
+        </BtnNeutral>
+        <BtnPurple disabled={clientSecret.isLoading} type="submit" fullWidth>
+          Save
+        </BtnPurple>
+      </div>
+    </>
   );
 };

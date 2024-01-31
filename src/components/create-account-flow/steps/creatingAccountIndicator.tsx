521b4ee536f699b0e1815964240d80b7740d829a
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2024-01-31 12:09:55 -0600

    basic implemetnation for creating account

--- src/components/create-account-flow/steps/creatingAccountIndicator.tsx
+++ src/components/create-account-flow/steps/creatingAccountIndicator.tsx
@@ -1,13 +1,57 @@
-import { useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { api } from "../../../server/utils/api";
 import { CircularProgress } from "../../circularProgress";
+import { CreateOrgContext } from "../dataStore";
 
 // TODO: add in ability to create org on load
 // need server functionallity linked in
-export const CreateAccountIdentifier = () => {
-  const [text, setText] = useState("Getting everything ready...");
-  // useEffect(() => {
-  //   setTimeout(() => {setText("creating your account... ")}, 2000)
-  // })
+export const CreateAccountIdentifier = ({
+  setStep,
+}: {
+  setStep: Dispatch<SetStateAction<number>>;
+}) => {
+  const { state, setState } = useContext(CreateOrgContext)!;
+  const createOrg = api.organization.createOrg.useMutation();
+  const [text, setText] = useState("Setting up your organization...");
+
+  // used for creating the user and the organziation and then continueing to the pricing tier phase
+  useEffect(() => {
+    createOrg.mutate(
+      {
+        firstName: state.firstName,
+        lastName: state.lastName,
+        email: state.email,
+        phoneNumber: state.phoneNumber,
+        password: state.password,
+        status: "ADMIN",
+        orgName: state.orgName,
+        orgPhoneNumber: state.orgPhoneNumber,
+        stripeCustomerId: state.stripeCustomerId,
+      },
+      {
+        onSuccess(data, _variables, _context) {
+          if (data.org && data.org.id) {
+            // update state to include the org id.
+            // This allows me to easily attach the org ID to the stripe customer object
+            setState((prev) => ({ ...prev, orgId: data!.org!.id }));
+            setStep(4);
+          }
+        },
+        onError(error, _variables, _context) {
+          //TODO: implement a better system for displaying errors
+          //this can be done alert provider and Error alert and Success alert I have already made
+          alert("Error creating your account. Please try again.");
+          setStep(2);
+        },
+      }
+    );
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center">
       <CircularProgress />

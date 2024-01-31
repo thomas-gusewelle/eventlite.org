d32e15a0c5a48b72cea8f45288dab3ecef6b91a6
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2024-01-31 11:37:14 -0600

    need to test this

--- src/components/create-account-flow/steps/cardInfo.tsx
+++ src/components/create-account-flow/steps/cardInfo.tsx
@@ -1,20 +1,23 @@
-import { Elements, PaymentElement } from "@stripe/react-stripe-js";
-import { Dispatch, SetStateAction } from "react";
+import {
+  Elements,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { Dispatch, SetStateAction, useContext, useState } from "react";
 import { loadedStripe } from "../../../server/stripe/client";
 import { api } from "../../../server/utils/api";
 import { BtnNeutral } from "../../btn/btnNeutral";
 import { BtnPurple } from "../../btn/btnPurple";
 import { CardHeader } from "../components/cardHeader";
+import { CreateOrgContext } from "../dataStore";
 export const CardInfoSection = ({
   setStep,
-  stripeCustomerId
+  stripeCustomerId,
 }: {
-  setStep: Dispatch<SetStateAction<number>>
-  stripeCustomerId: string
+  setStep: Dispatch<SetStateAction<number>>;
+  stripeCustomerId: string;
 }) => {
-
-  const clientSecret = api.stripe.createSetupIntent.useQuery({customerId: stripeCustomerId})
-
   return (
     <>
       <CardHeader>Setup your payment method</CardHeader>
@@ -22,22 +25,92 @@ export const CardInfoSection = ({
         stripe={loadedStripe}
         options={{ mode: "setup", currency: "usd" }}
       >
-        <PaymentElement />
+        <CardForm setStep={setStep} customerId={stripeCustomerId} />
       </Elements>
+    </>
+  );
+};
+
+const CardForm = ({
+  setStep,
+  customerId,
+}: {
+  setStep: Dispatch<SetStateAction<number>>;
+  customerId: string;
+}) => {
+  const {state} = useContext(CreateOrgContext)!
+  const stripe = useStripe();
+  const elements = useElements();
+  const clientSecret = api.stripe.createOrRetrieveSetupIntent.useQuery(
+    {
+      customerId: customerId,
+    },
+    {
+      onSuccess(data) {
+        if (typeof data == "string") {
+          setSecret(data);
+        }
+      },
+    }
+  );
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [secret, setSecret] = useState("");
+
+  const handleSubmit = async (e: FormDataEvent) => {
+    e.preventDefault();
+
+    if (!stripe || !elements || secret === "") {
+      return;
+    }
+    setIsLoading(true);
+    //TODO: Finish the handle submit for adding a card to user
+    const { error: submitError } = await elements.submit();
+
+    if (submitError) {
+      setIsLoading(false);
+      console.error(submitError);
+      return;
+    }
+
+    const { error } = await stripe.confirmSetup({
+      elements: elements,
+      clientSecret: secret,
+      confirmParams: {
+        return_url: `${window.location.origin}/create-account/setup?name=${state.name}`
+      }
+    });
+    if (error) {
+      setIsLoading(false)
+      console.error(error)
+      return;
+    } else {
+      setStep(5)
+    }
+  };
+
+  return (
+    <form>
+      <PaymentElement />
       <div className="mt-6 flex items-center justify-center gap-6">
         <BtnNeutral
           fullWidth
           func={() => {
             console.log("back being called");
-            setStep(3)
+            setStep(3);
           }}
         >
           Back
         </BtnNeutral>
-        <BtnPurple disabled={clientSecret.isLoading} type="submit" fullWidth>
+        <BtnPurple
+          disabled={clientSecret.isLoading}
+          isLoading={isLoading}
+          type="submit"
+          fullWidth
+        >
           Save
         </BtnPurple>
       </div>
-    </>
+    </form>
   );
 };

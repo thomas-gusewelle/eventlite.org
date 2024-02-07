dcd81f5c7332da5a2fa5c5352465f3a66e799493
Thomas Gusewelle <thomasgusewelle21@gmail.com>
2024-02-06 20:04:19 -0600

    update classes to make disabled visible

--- src/components/btn/btnPurple.tsx
+++ src/components/btn/btnPurple.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { classNames } from "../../utils/classnames";
 import { CircularProgressSmall } from "../circularProgress";
 
 export const BtnPurple = ({
@@ -21,9 +22,13 @@ export const BtnPurple = ({
       disabled={disabled}
       type={type}
       tabIndex={1}
-      className={`flex w-full justify-center rounded-lg border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2  sm:text-sm ${fullWidth ? "" : "sm:w-auto"
-        }`}
-      onClick={onClick}>
+      className={classNames(
+        `flex w-full justify-center rounded-lg border border-transparent  px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2  sm:text-sm`,
+        fullWidth ? "" : "sm:w-auto",
+        disabled ? "bg-indigo-400/90" : "bg-indigo-600"  
+      )}
+      onClick={onClick}
+    >
       {isLoading ? <CircularProgressSmall /> : children}
     </button>
   );

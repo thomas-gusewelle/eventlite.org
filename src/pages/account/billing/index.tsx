c87318777139044c3f585b10ee79004e084691ce
Thomas <tgusewelle@gkwmedia.com>
2024-02-12 03:28:13 +0000

    create page

---
+++ src/pages/account/billing/index.tsx
@@ -0,0 +1,8 @@
+import { sidebar } from "../../../components/layout/sidebar";
+
+const BillingIndex = () => {
+  return <div>Hello</div>
+}
+
+BillingIndex.getLayout = sidebar; 
+export default BillingIndex;

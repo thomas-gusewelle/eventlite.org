d5362854ebd2ac7c59ae160d7b1f44e01b6d500e
Thomas <tgusewelle@gkwmedia.com>
2024-02-14 19:10:39 +0000

    initial loading of invoices

--- src/pages/account/billing/history.tsx
+++ src/pages/account/billing/history.tsx
@@ -1,9 +1,25 @@
-import { BillingLayout } from "../../../components/layout/billing/layout"
-import { sidebar } from "../../../components/layout/sidebar"
+import { BillingLayout } from "../../../components/layout/billing/layout";
+import { sidebar } from "../../../components/layout/sidebar";
+import { api } from "../../../server/utils/api";
+import { CircularProgress } from "../../../components/circularProgress";
 
 const BillingHistoryPage = () => {
-  return <BillingLayout><div>Hello History</div></BillingLayout>
-}
 
-BillingHistoryPage.getLayout = sidebar
-export default BillingHistoryPage
+  const invoicesQuery = api.stripe.getAllInvoices.useQuery({});
+
+  if (invoicesQuery.isLoading) {
+  return <BillingLayout>
+      <div className="flex items-center justify-center"><CircularProgress/></div>
+    </BillingLayout>
+  }
+
+  return (
+    <BillingLayout>
+      {invoicesQuery?.data?.data?.map(d => 
+      <div>{d.id}</div>)}
+    </BillingLayout>
+  );
+};
+
+BillingHistoryPage.getLayout = sidebar;
+export default BillingHistoryPage;

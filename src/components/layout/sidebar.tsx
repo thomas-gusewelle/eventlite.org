8860cb8f96bdb5a67500ec71d1be1b2678d6816f
Thomas <tgusewelle@gkwmedia.com>
2024-02-21 09:40:48 -0600

    update text to billing on desktop

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -481,7 +481,7 @@ export const SidebarLayout: React.FC<{ children: any }> = ({ children }) => {
                                           : "text-gray-700",
                                         "block px-4 py-2 text-sm"
                                       )}>
-                                      Settings
+                                      Billing
                                     </Link>
                                   </button>
                                 )}

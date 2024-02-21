7b488b60a338f7af849153eaff36e5e42437b14c
Thomas <tgusewelle@gkwmedia.com>
2024-02-21 09:38:02 -0600

    wip

--- src/pages/account/billing/settings.tsx
+++ src/pages/account/billing/settings.tsx
@@ -1,10 +1,14 @@
+import { SectionHeading } from "../../../components/headers/SectionHeading";
 import { BillingLayout } from "../../../components/layout/billing/layout";
 import { sidebar } from "../../../components/layout/sidebar";
 
 const BillingSettings = () => {
   return (
     <BillingLayout>
-      <div>Hello Billing Settings</div>
+      <section>
+        {/*TODO: get default payment info and display to user*/}
+        <SectionHeading>Billing Information</SectionHeading>
+      </section>
     </BillingLayout>
   );
 };

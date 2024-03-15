b38cd19dc90840b127f76051c9e4cac7bab3ca4e
Thomas <tgusewelle@gkwmedia.com>
2024-03-14 19:48:03 -0500

    start of payment method

--- src/components/stripe/paymentMethod.tsx
+++ src/components/stripe/paymentMethod.tsx
@@ -1,6 +1,7 @@
 import { api } from "../../server/utils/api";
 
+//TODO: get default payment info and display to user
 export const PaymentMethod = () => {
-  const [pm, pmQuery] = api.stripe.getDefaultPaymentMethod.useSuspenseQuery();
-  return <>{pm.paymentMethod ?? "null"}</>;
+  const [pm, _pmQuery] = api.stripe.getDefaultPaymentMethod.useSuspenseQuery();
+  return <>{pm.paymentMethod ?? null}</>;
 };

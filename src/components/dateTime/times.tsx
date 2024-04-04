abe07393f3a29e645c63ec72b00052c9828cbd2c
Thomas <tgusewelle@gkwmedia.com>
2024-04-04 12:38:21 -0500

    try this

--- src/components/dateTime/times.tsx
+++ src/components/dateTime/times.tsx
@@ -1,5 +1,14 @@
-export function shortTime(time: Date, timeZone?: string, offset: number = new Date().getTimezoneOffset()) {
-  const tempTime = new Date(time);
+export function shortTime(
+  time: Date,
+  timeZone?: string,
+  offset: number = new Date().getTimezoneOffset()
+) {
+  const tempTime = new Date(
+    time.toLocaleString(undefined, { timeZone: timeZone })
+  );
   tempTime.setMinutes(tempTime.getTimezoneOffset() - offset);
-  return Intl.DateTimeFormat("en-US", { timeStyle: "short", timeZone: timeZone }).format(tempTime);
+  return Intl.DateTimeFormat("en-US", {
+    timeStyle: "short",
+    timeZone: timeZone,
+  }).format(tempTime);
 }

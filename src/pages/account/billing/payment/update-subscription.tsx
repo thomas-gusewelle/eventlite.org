6d1140a4b9e396848495ed4408f5577c0e20cd29
Thomas <tgusewelle@gkwmedia.com>
2024-04-11 14:53:16 -0500

    create page

---
+++ src/pages/account/billing/payment/update-subscription.tsx
@@ -0,0 +1,18 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { sidebar } from "../../../../components/layout/sidebar";
+
+export const getServerSideProps = (context: GetServerSidePropsContext) =>  {
+const setupId = context.params
+  //get setupIntent
+
+  //set customer default to payment method
+  //make sure to also set customer.invoice_setting.default_payment_method
+}
+
+const UpdateSubscriptionPage = () => {
+
+}
+
+UpdateSubscriptionPage.getLayout = sidebar;
+export default UpdateSubscriptionPage;
+
